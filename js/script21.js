a4b9b103b93f6d3316f055a8422a9dd00e7d50ee
Tony Makagon <makagontony@gmail.com>
2021-04-16 01:16:56 -0400

    Merged Victors code with current
    Fully updated

---
+++ js/script21.js
@@ -0,0 +1,20 @@
+import Deck from "./deck.js"
+
+const CARD_VALUE_MAP = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14
+}
+
+const text = document.querySelector('.top-text21')
+const deckElement = document.querySelector('.deck21-slot')
